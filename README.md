ce0fddfa3938b1466a31a0511a64c6b4e84a0081
Skarphéðinn Þórðarson <skarphedinnth@gmail.com>
2019-11-03 20:54:46 +0000

    Adding code

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # eslint-plugin-the-step-down-rule
+
 Enforces that functions follow a top-down call structure when calling a lower-level function.
+
+This is basically just the opposite of the `no-use-before-define` rule, except it only applies to functions that are called outside the scope they are defined in.
+
+## Example
+
+##### Good
+```js
+const a = require('a');
+
+function b() {
+  a();
+  d();
+}
+
+function c() {
+  d();
+}
+
+const d = () => {};
+
+b();
+```
+
+##### Bad
+```js
+function a() {
+  b();
+}
+
+const b = () => {};
+
+function c() => {
+  b();
+}
+```
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-the-step-down-rule`:
+
+```
+$ npm install eslint-plugin-the-step-down-rule --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-the-step-down-rule` globally.
+
+## Usage
+
+Add `the-step-down-rule` to your `.eslintrc` configuration file.
+```
+{
+    "plugins": [
+        "the-step-down-rule"
+    ],
+    "rules": {
+        "the-step-down-rule/the-step-down-rule": "warn" || "err"
+    }
+}
+```
+
+## Additional Rules
+
+Depending on your setup, you might need to disable the `no-use-before-define` rule.
+```
+"no-use-before-define": ["error", { "variables": false, "functions": false }]
+```
---
+++ tests/index.js
@@ -0,0 +1,192 @@
+const fs = require("fs");
+const RuleTester = require("eslint").RuleTester;
+
+const rule = require("../lib/rules/the-step-down-rule");
+
+const errMsg = "Does not follow a top-down call structure";
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  }
+});
+
+ruleTester.run("the-step-down-rule", rule, {
+  valid: [
+    { code: fs.readFileSync("./lib/rules/the-step-down-rule.js", "utf8") },
+    {
+      code: `
+        const a = require('aaa');
+  
+        function b() {
+          a();
+        }
+      `,
+    },
+    {
+      code: `
+        const { a } = require('a');
+
+        function b() {
+          a();
+        }
+      `,
+    },
+    {
+      code: `
+        import a from 'a';
+
+        function b() {
+          a();
+        }
+      `,
+    },
+    {
+      code: `
+        function a() {
+          b();
+        }
+
+        function b() {}
+      `,
+    },
+    {
+      code: `
+        const a = () => {
+          b();
+        }
+
+        const b = () => {}
+      `,
+    },
+    {
+      code: `
+        function a() {
+          b();
+        }
+
+        const b = () => {}
+      `,
+    },
+    {
+      code: `
+        const a = () => {
+          b();
+        }
+
+        function b() {}
+      `,
+    },
+    {
+      code: `
+        const a = () => {
+          a();
+        }
+      `,
+    },
+    {
+      code: `
+        function a() {
+          a();
+        }
+      `,
+    },
+    {
+      code: `
+        const a = require('a');
+
+        function b() {
+          a();
+          d();
+        }
+        
+        function c() {
+          d();
+        }
+        
+        const d = () => {};
+        
+        b();
+      `,
+    },
+    {
+      code: `        
+        function a() {
+          b();
+        }
+        
+        function c() {
+          b();
+          d();
+        }
+        
+        function b() {
+          e();
+        }
+        
+        function e() {}
+        
+        function d() {}
+
+        a();
+        c();
+      `,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `
+        function b() {}
+
+        function a() {
+          b();
+        }
+      `,
+      errors: [{ message: `'b' ${errMsg}` }]
+    },
+    {
+      code: `
+        function a() {
+          b();
+        }
+        
+        function b() {}
+
+        const c = () => {
+          b();
+        }
+      `,
+      errors: [{ message: `'b' ${errMsg}` }]
+    },
+    {
+      code: `   
+        a();
+        c();
+        
+        function a() {
+          b();
+        }
+        
+        function c() {
+          b();
+          d();
+        }
+        
+        function e() {}
+
+        function b() {
+          e();
+        }
+                
+        function d() {}
+      `,
+      errors: [
+        { message: `'a' ${errMsg}` },
+        { message: `'c' ${errMsg}` },
+        { message: `'e' ${errMsg}` },
+      ]
+    }
+  ]
+});
